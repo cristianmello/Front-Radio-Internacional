1df044d1c0bef514ec04ebd4f1cc5e13ce34982a
cristianmello <mellocristian45@gmail.com>
2025-07-20 05:53:19 -0300

    defineconfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // opcional: si quieres exponer también el server de dev
+  server: {
+    host: true,
+  },
+  preview: {
+    // hace que escuche en 0.0.0.0
+    host: true,
+    // puerto opcional, Railway inyecta PORT
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 4173,
+    // aquí autorizas tu dominio
+    allowedHosts: [
+      'realidadnacional.net',
+      // si tienes subdominios o quieres todo, puedes usar:
+      // '.realidadnacional.net'
+      // o
+      // 'all'
+    ],
+  },
 })
