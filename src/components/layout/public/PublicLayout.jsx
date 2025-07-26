b5f4e9cbe4cdc09b83bac26dc672cd63dd3c7739
cristianmello <mellocristian45@gmail.com>
2025-07-25 21:09:35 -0300

    fix import useLocation

--- src/components/layout/public/PublicLayout.jsx
+++ src/components/layout/public/PublicLayout.jsx
@@ -1,6 +1,6 @@
 // src/components/layout/public/PublicLayout.jsx
 import React, { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import AuthModal from './AuthModal';
